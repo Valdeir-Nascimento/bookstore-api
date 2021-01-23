c9651eaa978f5c990d4b85a7de1341dbfddbd950
valdeir.nascimento <valdeircn11@gmail.com>
2021-01-22 22:43:32 -0300

    Configuração da base de dados de forma automatica

--- src/main/java/com/nascimeto/bookstore/BookstoreApiApplication.java
+++ src/main/java/com/nascimeto/bookstore/BookstoreApiApplication.java
@@ -1,13 +1,41 @@
 package com.nascimeto.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApiApplication {
+import com.nascimeto.bookstore.domain.Categoria;
+import com.nascimeto.bookstore.domain.Livro;
+import com.nascimeto.bookstore.repositories.CategoriaRepository;
+import com.nascimeto.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", c1);
+		c1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(c1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
+		
 	}
 
 }
--- src/main/java/com/nascimeto/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/nascimeto/bookstore/repositories/LivroRepository.java
@@ -1,5 +1,11 @@
 package com.nascimeto.bookstore.repositories;
 
-public class LivroRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.nascimeto.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long>{
+	
 }
