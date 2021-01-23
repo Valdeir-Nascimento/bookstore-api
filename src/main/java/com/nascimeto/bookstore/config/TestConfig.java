18a3274baeb01705a730c5cfea4c70bf7b816863
valdeir.nascimento <valdeircn11@gmail.com>
2021-01-22 23:21:17 -0300

    Criação de perfil de test.

---
+++ src/main/java/com/nascimeto/bookstore/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.nascimeto.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nascimeto.bookstore.service.DBService;
+
+
+
+@Configuration
+@Profile("dev")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instaciaBaseDeDados( ) {
+		this.dbService.instanciaBaseDeDados();
+	}
+	
+}
